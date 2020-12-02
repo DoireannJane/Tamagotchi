4f8fa21f4c6b04411ad70f2065d349069c68210f
Doireann Herold <ivydoor@icloud.com>
2020-12-02 12:42:42 -0800

    Age counter console functional

--- app.js
+++ app.js
@@ -1,16 +1,55 @@
 console.log("Welcome proud parent! Click egg to hatch!")
 
-$("#egg").click(function(){
+const gameBegin = $("#egg").click(function(){
     $("#egg").hide();
+    let age = 1;
+    const ager = setInterval(function (){
+        console.log(`It's my ${age} Birthday!`);
+        age++;
+        if (age === 11){
+            clearInterval(ager);
+        }
+    }, 1000);
+});
 
+// let age = 1;
+// const setAger = function setAger() {
+//     const ager = setInterval(function (){
+//         console.log(`It's my ${age} Birthday!`);
+//         age++;
+//         if (age === 11){
+//             clearInterval(ager);
+//         }
+//     }, 1000);
+// }
+
+
+
+//  $("#egg").click(function(){
+//     console.log(`It's my ${age} Birthday!`)
+//   });
+ 
+
+//   function becomeChild(){
+//     $("#baby").hide();
+//   }
+//  const timer = setInterval(becomeChild, 1000);
+//  clearInterval(timer);
+
+$("#feed").click(function(){
+    console.log("Yum Yum says the Hydra!")
   });
 
-  function becomeChild(){
-    $("#baby").hide();
-  }
- const timer = setInterval(becomeChild, 1000);
- clearInterval(timer);
- 
+$("#play").click(function(){
+    console.log("Woohoo says the hydra")
+  });
+$("#sleep").click(function(){
+    console.log("ZzZzZ says the Hydra!")
+  });
+
+
+
+
 
  
  
