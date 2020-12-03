eb337593acc930a86ddaa09154b1cfafbbdf2120
Doireann Herold <ivydoor@icloud.com>
2020-12-03 00:03:13 -0800

    End of game partially complete- declining health established

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 console.log("Welcome proud parent! Click egg to hatch!")
+// $('input').keyup(function(event) {
+//     if (event.keyCode === 13) {
+//         $("#id_of_button").click();
+//     }
+// });
+// $('input').value.on
 let age = 0;
+let health = 10;
+let fun = 10;
+let energy = 10;
 const gameBegin = $("#egg").click(function(){
     $("#egg").hide();
     $("body").css("background-color","black");
     $(".age").css("color","white");
+    $(".overall").css("color","white");
     const ager = setInterval(function (){
         console.log(`It's my Birthday!`);
         age++;
@@ -12,14 +22,31 @@ const gameBegin = $("#egg").click(function(){
             $("#baby").hide();
             $("body").css("background-color","white");
             $(".age").css("color","black");
+            $(".overall").css("color","black");
         }
         if (age ===6){
             $("#child").hide();
             $("body").css("background-color","black");
             $(".age").css("color","white");
+            $(".overall").css("color","white");
         }
         if (age === 10){
             clearInterval(ager);
+        }
+    }, 1000);
+}
+);
+const overallDecrease = $("#egg").click(function(){
+    const healthMinus = setInterval(function (){
+        console.log(`health is declining`);
+        health--;
+        $(".overall").text(`health:${health}`);
+        if (health === 0) {
+            clearInterval(healthMinus);
+            $("#egg").hide();
+            $("#baby").hide();
+            $("#child").hide();
+            $("#adult").hide();
         }
     }, 2000);
 });
